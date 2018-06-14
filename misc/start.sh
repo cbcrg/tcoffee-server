8a73079bbc38f780d6c1aa2021151ba715feadcb
smoretti <smoretti@unil.ch>
2018-06-14 16:41:52 +0200

    Update start/stop script for daemon

--- misc/start.sh
+++ misc/start.sh
@@ -1,7 +1,6 @@
 cd /var/vhosts/tcoffee/
-sudo -u tcoffee sh -c 'source /mnt/common/etc/profile.d/vitalit_lsf.sh;
-                       source /etc/profile.d/vitalit.sh;
+sudo -u tcoffee sh -c 'source /etc/profile.d/vitalit.sh;
                        module add SequenceAnalysis/MultipleSequenceAlignment/T-Coffee/11.00.8cbe486;
-                       module add SequenceAnalysis/ProtoGene/4.2.0;
+                       module add SequenceAnalysis/ProtoGene/4.2.2;
                        /var/vhosts/tcoffee/play/play start ./tserver --%vital'
 cd - >/dev/null
